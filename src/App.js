237195d615a60006de6346e45d81576874cafd24
arunajoy2602 <arunajoy2602@gmail.com>
2021-01-09 09:56:26 +0100

    loginscreen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import Splash from './screens/Splash'
+import FirstSlide from './screens/FirstSlide';
+import SecondSlide from './screens/SecondSlide';
+import ThirdSlide from './screens/ThirdSlide';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginScreen from './screens/LoginScreen';
+
+const App = () => (
+  <div className="App">
+    <Router>
+      <Switch>
+        <Route path="/firstslide">
+          <FirstSlide />
+        </Route>
+        <Route path="/secondslide">
+          <SecondSlide />
+        </Route>
+        <Route path="/thirdslide">
+          <ThirdSlide />
+        </Route>
+        <Route path="/loginscreen">
+          <LoginScreen />
+        </Route>
+        <Route path="/">
+          <Splash />
+        </Route>
+      </Switch>
+    </Router>
+  </div>
+
+);
 
 export default App;
